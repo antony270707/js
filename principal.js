bfed53532942a749883d2361f02125fcf3311e5c
antony270707 <109869237+antony270707@users.noreply.github.com>
2023-09-15 09:05:22 -0300

    Update principal.js

--- principal.js
+++ principal.js
@@ -1,9 +1,13 @@
 var titulo = document.querySelector(".titulo");
 titulo.textContent = "Aparecida Nutricionista";
 
-var paciente = document.querySelector("#primeiro-paciente");
+var paciente = document.querySelectorAll(".paciente");
 
-var tdPeso = paciente.querySelector(".info-peso");
+for(var i = 0; i < paciente.length ; i++){
+
+    var paciente = paciente[i];
+
+    var tdPeso = paciente.querySelector(".info-peso");
 var peso = tdPeso.textContent;
 
 var tdAltura = paciente.querySelector(".info-altura");
@@ -11,6 +15,23 @@ var altura = tdAltura.textContent;
 
 var tdImc = paciente.querySelector(".info-imc");
 
-var imc = peso / (altura * altura);
+var pesoEhValido = true;
+var alturaEhValida = true;
 
-tdImc.textContent = imc;
+if(peso <= 0 || peso >= 1000){
+    console.log("Peso invalido!");
+    pesoEhValido = false;
+    tdImc.textContent = "Peso invalido!";
+}
+
+if(altura <= 0 || altura >= 3.00){
+    console.log("Altura invalida!");
+    alturaEhValida = false;
+    tdImc.textContent = "Altura invalids!"
+}
+
+if( alturaEhValida && pesoEhValido){
+    var imc = peso / (altura * altura);
+    tdImc.textContent = imc;
+}
+}
