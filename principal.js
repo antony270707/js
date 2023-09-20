578601b605bbdcb53c7fb2eb2f01a74a8f0d722c
antony270707 <109869237+antony270707@users.noreply.github.com>
2023-09-20 09:50:11 -0300

    Update principal.js

--- principal.js
+++ principal.js
@@ -18,20 +18,25 @@ var tdImc = paciente.querySelector(".info-imc");
 var pesoEhValido = true;
 var alturaEhValida = true;
 
-if(peso <= 0 || peso >= 1000){
-    console.log("Peso invalido!");
-    pesoEhValido = false;
-    tdImc.textContent = "Peso invalido!";
+   if(peso <= 0 || peso >= 1000){
+       console.log("Peso invalido!");
+       pesoEhValido = false;
+       tdImc.textContent = "Peso invalido!";
+   }
+
+   if(altura <= 0 || altura >= 1.50){
+        console.log("Altura invalida!");
+        alturaEhValida = false;
+        tdImc.textContent = "Altura invalida!"
+   }
+
+    if( alturaEhValida && pesoEhValido){
+        var imc = peso / (altura * altura);
+        tdImc.textContent = imc;
+   }
 }
 
-if(altura <= 0 || altura >= 3.00){
-    console.log("Altura invalida!");
-    alturaEhValida = false;
-    tdImc.textContent = "Altura invalids!"
-}
-
-if( alturaEhValida && pesoEhValido){
-    var imc = peso / (altura * altura);
-    tdImc.textContent = imc;
-}
-}
+var botaoAdicionar = document.querySelector("#adicionar-paciente");
+botaoAdicionar.addEventListener("click",function(){
+    console.log("Oi, cliquei no botão")
+});
